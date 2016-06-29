67f710c404d60294cceaa00487a8eed023787648
Alexander Rebru <genarido@yandex.ru>
2016-06-29 20:15:48 +0400

    Update plugin_titlelink.py
    Удаление мусора, описание ошибок.

--- plugin_titlelink.py
+++ plugin_titlelink.py
@@ -1,4 +1,5 @@
 # -*-coding:utf-8 -*-
+
 import re
 from ritsu_api import *
 from ritsu_utils import *
@@ -26,17 +27,17 @@ def event_room_message(bot, (message, room, nick)):
       return
     regexp = r'(?:http[s]?://[\w\d\-.]*\.[\w\d]*[:\d]*[/]?[/&=\$\-\~\.\+\!\?\*\'\(\)\,\%\w]*)'
     link = re.findall(regexp, text)
-    #link = re.findall(r'(:(:https|ftp)://)(:\S+(::\S*)@)(:(!10(:\.\d{1,3}){3})(!127(:\.\d{1,3}){3})(!169\.254(:\.\d{1,3}){2})(!192\.168(:\.\d{1,3}){2})(!172\.(:1[6-9]|2\d|3[0-1])(:\.\d{1,3}){2})(:[1-9]\d|1\d\d|2[01]\d|22[0-3])(:\.(:1\d{1,2}|2[0-4]\d|25[0-5])){2}(:\.(:[1-9]\d|1\d\d|2[0-4]\d|25[0-4]))|(:(:[a-z\x{00a1}-\x{ffff}0-9]+-)*[a-z\x{00a1}-\x{ffff}0-9]+)(:\.(:[a-z\x{00a1}-\x{ffff}0-9]+-)*[a-z\x{00a1}-\x{ffff}0-9]+)*(:\.(:[a-z\x{00a1}-\x{ffff}]{2,})))(::\d{2,5})(:/[^\s]*)$_iuS', text)
+    
     if link:
       try:
 	source = get_link_title(link[0])
       except Exception, e:
-	pass
+	return "Error get_link_title()."
       try:
 	res = "%s"%(source)
 	bot.send_room_message(target, res)
       except Exception, e:
-	return "e"
+	return "Other error."
 
 def get_title(rec):
     soup = BeautifulSoup(rec)
